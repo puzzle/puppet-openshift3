fe632c4f0b9fcca7860cc42cf6196bec98ea48af
Daniel Tschan <tschan@puzzle.ch>
2015-10-21 16:00:47 +0200

    Make application domain and cluster subnet configurable. Fix identity provider deafult configuration.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,18 +1,20 @@
 class openshift3::params {
   $deployment_type = "origin"
-  $identity_providers = {
+  $identity_providers = [{
     'name' => 'htpasswd_auth',
     'login' => 'true',
     'challenge' => 'true',
     'kind' => 'HTPasswdPasswordIdentityProvider',
     'filename' => '/etc/openshift/openshift-passwd',
-  }
+  }]
   $master = undef
   $node_labels = {
     'region' => 'primary',
     'zone' => 'default',
   }
+  $app_domain = 'cloudapps.example.com'
   $openshift_dns_bind_addr = undef
   $package_version = undef
   $ssh_key = undef
+  $cluster_network_cidr = '10.1.0.0/16'
 }
