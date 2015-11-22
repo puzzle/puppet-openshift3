3c396d57fe8cf9717d8d0f118317ee266c59332a
Daniel Tschan <tschan@puzzle.ch>
2015-11-22 15:37:14 +0100

    Version lock Docker to prevent incompatibilities between OpenShift and Docker.

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,12 +13,19 @@ class openshift3 (
   if $deployment_type == "enterprise" {
     $component_prefix = 'registry.access.redhat.com/openshift3/ose'
     $package_name = 'openshift'
+    if versioncmp($version, '3.1.0') >= 0 {
+      $docker_version = '1.8.2'
+    } else {
+      $docker_version = '1.6.2'
+    }
   } else {
     $component_prefix = 'openshift/origin'
     if versioncmp($version, '1.0.5') > 0 {
       $package_name = 'origin'
+      $docker_version = '1.8.2'
     } else {
       $package_name = 'openshift'
+      $docker_version = '1.6.2'
     }
   }
   $component_images = "${component_prefix}-\${component}:\${version}"
