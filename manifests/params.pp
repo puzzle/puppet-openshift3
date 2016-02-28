1114e7a77afdac9817c49fbad0e3f97930d9351f
Daniel Tschan <tschan@puzzle.ch>
2016-02-28 17:47:49 +0100

    Remove no longer needed parameters from openshift3 class.

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,17 +7,10 @@ class openshift3::params {
     'kind' => 'HTPasswdPasswordIdentityProvider',
     'filename' => '/etc/openshift/openshift-passwd',
   }]
-  $master = undef
-  $node_labels = {
-    'region' => 'primary',
-    'zone' => 'default',
-  }
   $app_domain = 'cloudapps.example.com'
   $openshift_dns_bind_addr = undef
   $package_version = undef
-  $ssh_key = undef
   $cluster_network_cidr = '10.1.0.0/16'
-  $schedulable = true
   $configure_epel = true
   $no_proxy = "localhost,127.0.0.1"
   $install_router = true
