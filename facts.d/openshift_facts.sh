9dc709bc18d4e927412060f6a2520f5110cbbc86
Daniel Tschan <tschan@puzzle.ch>
2016-02-29 13:57:25 +0000

    Don't fail when OpenShift is not setup yet.

--- facts.d/openshift_facts.sh
+++ facts.d/openshift_facts.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
 
 cat <<EOF
-openshift_registry_ip=`oc get svc docker-registry -n default -o json | jq -r .spec.clusterIP`
+openshift_registry_ip=`oc get svc docker-registry -n default -o json 2>/dev/null | jq -r .spec.clusterIP 2>/dev/null`
 #openshift_version=`openshift version 2>/dev/null | sed -ne 's/openshift \+v\?\([0-9.]\+\).*/\1/p'`
 #openshift_registry_version=`oc get dc/docker-registry -n default -o json 2>/dev/null |sed -ne 's/.*"image": *"[^:]\+:v\?\([0-9.]\+\).*/\1/p'`
 #openshift_router_version=`oc get dc/router -n default -o json 2>/dev/null |sed -ne 's/.*"image": *"[^:]\+:v\?\([0-9.]\+\).*/\1/p'`
