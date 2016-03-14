f03979a12ef2a7f823d460ba6c42cbb4176b7e8a
Tobias Brunner <tobias@tobru.ch>
2016-03-14 20:24:55 +0100

    more robust facts

--- facts.d/openshift_facts.sh
+++ facts.d/openshift_facts.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
 
-if [ $(which oc) ]; then
+if which oc; then
 cat <<EOF
 openshift_registry_ip=`oc get svc docker-registry -n default -o json 2>/dev/null | jq -r .spec.clusterIP 2>/dev/null`
-#openshift_version=`openshift version 2>/dev/null | sed -ne 's/openshift \+v\?\([0-9.]\+\).*/\1/p'`
-#openshift_registry_version=`oc get dc/docker-registry -n default -o json 2>/dev/null |sed -ne 's/.*"image": *"[^:]\+:v\?\([0-9.]\+\).*/\1/p'`
-#openshift_router_version=`oc get dc/router -n default -o json 2>/dev/null |sed -ne 's/.*"image": *"[^:]\+:v\?\([0-9.]\+\).*/\1/p'`
+openshift_registry_version=`oc get dc/docker-registry -n default -o json 2>/dev/null |sed -ne 's/.*"image": *"[^:]\+:v\?\([0-9.]\+\).*/\1/p'`
+openshift_router_version=`oc get dc/router -n default -o json 2>/dev/null |sed -ne 's/.*"image": *"[^:]\+:v\?\([0-9.]\+\).*/\1/p'`
+EOF
+else
+cat <<EOF
+openshift_registry_ip=
+openshift_registry_version=
+openshift_router_version=
+EOF
+fi
+
+if which openshift; then
+cat <<EOF
+openshift_version=`openshift version 2>/dev/null | sed -ne 's/openshift \+v\?\([0-9.]\+\).*/\1/p'`
+EOF
+else
+cat <<EOF
+openshift_version=
 EOF
 fi
