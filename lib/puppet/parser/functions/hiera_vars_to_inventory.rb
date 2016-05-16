60169d7e663cd30453e01886a58582ba1dbac8d8
Daniel Tschan <tschan@puzzle.ch>
2016-05-16 19:30:31 +0200

    Add support to configure arbitrary Ansible variables.

---
+++ lib/puppet/parser/functions/hiera_vars_to_inventory.rb
@@ -0,0 +1,16 @@
+module Puppet::Parser::Functions
+  newfunction(:hiera_vars_to_inventory, :type => :rvalue) do |args|
+    result = ""
+    args[0].each do |key, value|
+      if value.is_a?(TrueClass) || value.is_a?(FalseClass)
+        result += "#{key}=#{value}\n"
+      elsif value.is_a?(String)
+       result += "#{key}='#{value}'\n"
+      else
+        result += "#{key}=#{value.to_json}\n"
+      end
+    end
+
+    return result
+  end
+end
