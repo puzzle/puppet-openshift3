86466598c33716d3eda9377d73db0e33dd2a485f
Daniel Tschan <tschan@puzzle.ch>
2016-06-20 13:54:31 +0200

    Remove trailing newlines from Ansible variable values.

--- lib/puppet/parser/functions/hiera_vars_to_inventory.rb
+++ lib/puppet/parser/functions/hiera_vars_to_inventory.rb
@@ -5,7 +5,7 @@ module Puppet::Parser::Functions
       if value.is_a?(TrueClass) || value.is_a?(FalseClass)
         result += "#{key}=#{value}\n"
       elsif value.is_a?(String)
-       result += "#{key}='#{value}'\n"
+       result += "#{key}='#{value.chomp}'\n"
       else
         result += "#{key}=#{value.to_json}\n"
       end
