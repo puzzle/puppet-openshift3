f85f97885196ea629f70eed9a754887f598b6f32
Daniel Tschan <tschan@puzzle.ch>
2016-06-23 17:26:39 +0200

    Update logging for OSE 3.2.

--- manifests/new_project.pp
+++ manifests/new_project.pp
@@ -1,9 +1,9 @@
-define openshift3::new_project () {
+define openshift3::new_project ($options = '') {
   exec { "new project ${title}":
     provider    => 'shell',
     environment => 'HOME=/root',
     cwd         => "/root",
-    command     => "oadm new-project ${title}",
+    command     => "oadm new-project ${title} ${options}",
     unless      => "oc get project ${title}",
     timeout     => 60,
     path        => $::path,
