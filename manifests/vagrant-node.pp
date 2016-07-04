d756673cbb9cbb0c59a0df117b3f238517563d1d
Daniel Tschan <tschan@puzzle.ch>
2016-07-04 17:01:36 +0200

    [blacksmith] Bump version to 1.0.10

---
+++ manifests/vagrant-node.pp
@@ -0,0 +1,31 @@
+class openshift3::vagrant-node {
+
+  if $::vagrant {
+    $openshift_hosts = parsejson($::openshift_hosts)
+    $master_ip = $openshift_hosts[0]['ip']
+
+    class { '::ntp':
+      servers => [ '0.rhel.pool.ntp.org', '1.rhel.pool.ntp.org', '2.rhel.pool.ntp.org' ],
+    }
+
+    file { '/etc/hosts':
+      ensure  => present,
+       owner  => 'root',
+      group  => 'root',
+      mode   => 0644,
+      content => template("openshift3/etc/hosts.erb"),
+    }
+
+    class { 'resolv_conf':
+      domainname => '.',
+      nameservers => [$master_ip, '8.8.8.8', '8.8.4.4'],  # Use Google Public DNS as forwarder
+    }
+
+    # Prevent dhclient from overwriting puppet managed /etc/resolv.conf with DHCP provided DNS servers
+    augeas { "/etc/sysconfig/network-scripts/ifcfg-${::network_primary_interface}":
+      changes => [
+        "set /files/etc/sysconfig/network-scripts/ifcfg-${::network_primary_interface}/PEERDNS no",
+      ],
+    }
+  }
+}
