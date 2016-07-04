41ead81219af2fc9c017f6e198ffd56bf8e2da03
Daniel Tschan <tschan@puzzle.ch>
2016-07-04 16:46:26 +0200

    Fix file name of Ansible upgrade playbook logs.

--- manifests/run_upgrade_playbook.pp
+++ manifests/run_upgrade_playbook.pp
@@ -1,5 +1,6 @@
 define openshift3::run_upgrade_playbook($if_deployment_type, $if_cur_ver, $if_new_ver) {
   $cur_version_arr = split($::openshift_version, '\.')
+  $playbook = basename(dirname($title))
 
   if $::openshift3::deployment_type == $if_deployment_type and $::openshift_version != '' and $::openshift_version != $::openshift3::version and "${cur_version_arr[0]}.${cur_version_arr[1]}" == $if_cur_ver and "${::openshift3::major}.${::openshift3::minor}" == $if_new_ver {
     ensure_resource('file', '/var/lib/puppet-openshift3/playbooks', { ensure => directory })
