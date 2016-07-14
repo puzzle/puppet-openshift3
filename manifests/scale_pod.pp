6ea30895fed84887ea385b5d203953b7cbe50fc6
Daniel Tschan <tschan@puzzle.ch>
2016-07-14 13:40:17 +0200

    Only scale fluend if it's deployed with a deploymentconfig.

--- manifests/scale_pod.pp
+++ manifests/scale_pod.pp
@@ -11,8 +11,8 @@ define openshift3::scale_pod ($namespace = 'default', $pod = $title, $replicas) 
     cwd         => "/root",
     command     => "READY_NODES=`oc get nodes -o json 2>/dev/null |jq -r '.items[] | select(.spec.unschedulable!=true and .status.conditions[0].status==\"True\").spec.externalID' 2>/dev/null | wc -l` && \
                     oc scale -n ${namespace} dc ${pod} --replicas=${real_replicas}",
-    unless      => "READY_NODES=`oc get nodes -o json 2>/dev/null |jq -r '.items[] | select(.spec.unschedulable!=true and .status.conditions[0].status==\"True\").spec.externalID' 2>/dev/null | wc -l` && \
-                   [ `oc get -n ${namespace} dc ${pod} -o json | jq .spec.replicas` = \"${real_replicas}\" ]",
+    unless      => "! oc get -n ${namespace} dc ${pod} >/dev/null 2>&1 || ( READY_NODES=`oc get nodes -o json 2>/dev/null |jq -r '.items[] | select(.spec.unschedulable!=true and .status.conditions[0].status==\"True\").spec.externalID' 2>/dev/null | wc -l` && \
+                   [ `oc get -n ${namespace} dc ${pod} -o json | jq .spec.replicas` = \"${real_replicas}\" ] )",
     timeout     => 60,
     logoutput   => on_failure,
     path        => $::path,
