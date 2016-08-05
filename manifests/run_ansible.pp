c0571041d2c2f58d90e700f1aeb46d01c751bea0
Daniel Tschan <tschan@puzzle.ch>
2016-08-05 15:42:41 +0200

    Also run pre-install playbook when new package updates are available to update versionlocks.

--- manifests/run_ansible.pp
+++ manifests/run_ansible.pp
@@ -1,14 +1,14 @@
-define openshift3::run_ansible($cwd, $options = '') {
+define openshift3::run_ansible($cwd, $options = '', $check_options = '') {
   exec { "Running ansible-playbook $title $options":
     command => "echo Running Ansible playbook $title",
-    unless  => "/var/lib/puppet-openshift3/ansible/run-ansible -c $title $options",
+    unless  => "/var/lib/puppet-openshift3/ansible/run-ansible -c $check_options $title $options",
     path => $::path,
   } ->
 
   exec { "ansible-playbook $title":
     cwd     => $cwd,
-    command => "/var/lib/puppet-openshift3/ansible/run-ansible $title $options",
-    unless  => "/var/lib/puppet-openshift3/ansible/run-ansible -c $title $options",
+    command => "/var/lib/puppet-openshift3/ansible/run-ansible $check_options $title $options",
+    unless  => "/var/lib/puppet-openshift3/ansible/run-ansible -c $check_options $title $options",
     timeout => 1000,
     logoutput => on_failure,
     path => $::path,
