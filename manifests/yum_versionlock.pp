eb36476ce5cfdc04a4b2b7f626aff07112af068d
Daniel Tschan <tschan@puzzle.ch>
2016-08-10 23:09:45 +0200

    Improve yum version locking.

--- manifests/yum_versionlock.pp
+++ manifests/yum_versionlock.pp
@@ -1,10 +1,12 @@
 define openshift3::yum_versionlock ($ensure = unset, $yum_options = "") {
   ensure_packages('yum-plugin-versionlock')
 
+  $nvr = inline_template("<%= `repoquery --queryformat '%{nvr}' ${yum_options} ${title}-${ensure}`.chomp %>")
+
   exec { "yum_versionlock $title":
     provider => 'shell',
-    command => "yum versionlock delete ${title}; yum ${yum_options} versionlock ${title}-${ensure}; yum versionlock list | grep -q :${title}-${ensure}",
-    unless => "yum versionlock list | grep -q :${title}-${ensure}",
+    command => "yum versionlock delete ${title}; yum ${yum_options} versionlock ${nvr}; yum versionlock list -q | grep -q ':${nvr}'",
+    unless => "yum versionlock list -q | grep -q :${nvr}",
     logoutput => on_failure,
     path => $::path,
     require => Package['yum-plugin-versionlock'],
