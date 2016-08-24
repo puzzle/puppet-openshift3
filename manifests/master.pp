5cf3be215b0b197cfec27f2ca77788e0561fe4cb
Daniel Tschan <tschan@puzzle.ch>
2016-08-24 21:53:36 +0200

    Don't manage atomic-openshift-master service on control machine as it may not be a master. No longer manage proxy config on master, configurable via Ansible playbook now.

--- manifests/master.pp
+++ manifests/master.pp
@@ -4,12 +4,10 @@ class openshift3::master {
   class { 'openshift3::package': } ->
   class { 'openshift3::vagrant-master': } ->
   class { 'openshift3::ansible': } ->
-  class { 'openshift3::proxy-master': } ->
   class { 'openshift3::router': } ->
   class { 'openshift3::failover': } ->
   class { 'openshift3::registry': } ->
   class { 'openshift3::metrics': } ->
   class { 'openshift3::logging': } ->
-  class { 'openshift3::monitoring': } ->
-  class { 'openshift3::service-master': }
+  class { 'openshift3::monitoring': }
 }
