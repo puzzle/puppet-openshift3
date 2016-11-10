69a26be26d606eb306a86b6f48112fbce347e557
Daniel Tschan <tschan@puzzle.ch>
2016-11-10 10:07:27 +0100

    Use priority plugin when versionlocking.

--- manifests/yum_versionlock.pp
+++ manifests/yum_versionlock.pp
@@ -3,8 +3,8 @@ define openshift3::yum_versionlock ($ensure = unset, $yum_options = "") {
 
   exec { "yum_versionlock $title":
     provider => 'shell',
-    command => "export nvr=`repoquery --queryformat '%{nvr}' ${yum_options} ${title}-${ensure}`; yum versionlock delete ${title}; yum ${yum_options} versionlock \${nvr}; yum versionlock list -q | grep -q :\${nvr}",
-    unless => "export nvr=`repoquery --queryformat '%{nvr}' ${yum_options} ${title}-${ensure}`; yum versionlock list -q | grep -q :\${nvr}",
+    command => "export nvr=`repoquery --plugins --setopt=disableplugins=versionlock --queryformat '%{nvr}' ${yum_options} ${title}-${ensure}`; yum versionlock delete ${title}; yum ${yum_options} versionlock \${nvr}; yum versionlock list -q | grep -q :\${nvr}",
+    unless => "export nvr=`repoquery --plugins --setopt=disableplugins=versionlock --queryformat '%{nvr}' ${yum_options} ${title}-${ensure}`; yum versionlock list -q | grep -q :\${nvr}",
     logoutput => on_failure,
     path => $::path,
     require => Package['yum-plugin-versionlock'],
