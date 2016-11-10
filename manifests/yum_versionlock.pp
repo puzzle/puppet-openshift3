5f02ab638ceda2d87d978ce1768b48db6f583ef0
Daniel Tschan <tschan@puzzle.ch>
2016-11-10 16:07:26 +0100

    Use priority plugin when versionlocking.

--- manifests/yum_versionlock.pp
+++ manifests/yum_versionlock.pp
@@ -1,10 +1,13 @@
 define openshift3::yum_versionlock ($ensure = unset, $yum_options = "") {
   ensure_packages('yum-plugin-versionlock')
 
+  $to_envr = 'sed -ne \'s/^package:[ \t]\+\([^ \t]\+\)\.\([^ \t]\+\)[ \t]\+\([^ \t]\+\).*/\1-\3/p\''
+  $get_envr = "export envr=`yum -q deplist --disableplugin=versionlock ${yum_options} ${title}-${ensure} | head -1 | ${to_envr}`"
+
   exec { "yum_versionlock $title":
     provider => 'shell',
-    command => "export nvr=`repoquery --plugins --setopt=disableplugins=versionlock --queryformat '%{nvr}' ${yum_options} ${title}-${ensure}`; yum versionlock delete ${title}; yum ${yum_options} versionlock \${nvr}; yum versionlock list -q | grep -q :\${nvr}",
-    unless => "export nvr=`repoquery --plugins --setopt=disableplugins=versionlock --queryformat '%{nvr}' ${yum_options} ${title}-${ensure}`; yum versionlock list -q | grep -q :\${nvr}",
+    command => "${get_envr}; yum versionlock delete ${title}; yum ${yum_options} versionlock \${envr}; yum versionlock list -q | grep -q \${envr}",
+    unless => "${get_envr}; yum versionlock list -q | grep -q \${envr}",
     logoutput => on_failure,
     path => $::path,
     require => Package['yum-plugin-versionlock'],
