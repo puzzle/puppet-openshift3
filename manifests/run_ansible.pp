f1e94c6ddda24fbe4c1fd4d10a5c5082d9a1ee8f
Benjamin Affolter <bliemli@users.noreply.github.com>
2017-07-20 10:31:54 +0200

    Raise timeout for ansible playbook runs

--- manifests/run_ansible.pp
+++ manifests/run_ansible.pp
@@ -18,7 +18,7 @@ define openshift3::run_ansible($cwd, $options = '', $check_options = '', $assert
       cwd     => $cwd,
       command => "/var/lib/puppet-openshift3/ansible/run-ansible $check_options $title $options",
       unless  => "/var/lib/puppet-openshift3/ansible/run-ansible -c $check_options $title $options",
-      timeout => 1800,
+      timeout => 2400,
       logoutput => on_failure,
       path => $::path,
     }
